1293936685f573cef0fece98f41fbe6b1c86ec9d
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-01-30 13:27:53 +0530

    angular login and signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'sign-up', component: SignupComponent},{
+  path:'log-in', component:LoginPageComponent
+},{
+  path:'**', redirectTo:'log-in',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
